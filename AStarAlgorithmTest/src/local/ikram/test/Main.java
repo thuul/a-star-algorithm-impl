d68bf1d243b18cd618121f6df83b5d2d57f4af23
Ikram OpeOluwa Samaad <cm-ard@live.com>
2016-07-05 01:34:05 +0200

    2016-07-05 update

--- AStarAlgorithmTest/src/local/ikram/test/Main.java
+++ AStarAlgorithmTest/src/local/ikram/test/Main.java
@@ -27,6 +27,10 @@ public class Main {
      */
     public static void main(String[] args) {
 
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("shutting down application ....");
+        }));
+
         TilePathGraph.newInstance();
 
         try {
